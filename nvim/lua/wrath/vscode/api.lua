c747b5eb19cf52996b9b544915d12086471a5e31
Raphael Vaz <raphael@wrath-codes.com>
2024-03-22 11:29:32 -0300

    feat(commands): :sparkles: Add Server Commands
    Added ServerPublishFull, ServerPublishIncremental, ServerAddDeployment, ServerRemoveDeployment, ServerStart, ServerStop, ServerRestart, ServerTerminate, ServerRemove, ServerCreate and ServerAddLocal commands

--- nvim/lua/wrath/vscode/api.lua
+++ nvim/lua/wrath/vscode/api.lua
@@ -289,3 +289,27 @@ function RestHistoryClear() VSCodeNotify("rest-client.clear-history") end
 function RestSwitchEnvironment() VSCodeNotify("rest-client.switch-environment") end
 
 function RestSnippetGenerate() VSCodeNotify("rest-client.generate-codesnippet") end
+
+-- Servers Commands
+
+function ServerPublishFull() VSCodeNotify("server.publishFull") end
+
+function ServerPublishIncremental() VSCodeNotify("server.publishIncremental") end
+
+function ServerAddDeployment() VSCodeNotify("server.addDeployment") end
+
+function ServerRemoveDeployment() VSCodeNotify("server.removeDeployment") end
+
+function ServerStart() VSCodeNotify("server.start") end
+
+function ServerStop() VSCodeNotify("server.stop") end
+
+function ServerRestart() VSCodeNotify("server.restart") end
+
+function ServerTerminate() VSCodeNotify("server.terminate") end
+
+function ServerRemove() VSCodeNotify("server.remove") end
+
+function ServerCreate() VSCodeNotify("server.createServer") end
+
+function ServerAddLocal() VSCodeNotify("server.addLocation") end
