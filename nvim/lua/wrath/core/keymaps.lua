a9522d6416b2c3ece32c4cb030d461dbc8838896
Raphael Vaz <raphael@wrath-codes.com>
2024-05-09 09:36:42 -0300

    wip: moved nvim and vscode folder to regular nvim, deleted nvim.old folder

--- nvim/lua/wrath/core/keymaps.lua
+++ nvim/lua/wrath/core/keymaps.lua
@@ -15,11 +15,17 @@ keymap("n", "#", "#zz", opts)
 keymap("n", "g*", "g*zz", opts)
 keymap("n", "g#", "g#zz", opts)
 
+
+-- Keep window centered when going up/down
+keymap("n", "<C-d>", "<C-d>zz", opts)
+keymap("n", "<C-u>", "<C-u>zz", opts)
+
 -- Stay in indent mode
 keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 
 -- Move lines
+
 keymap("v", "J", ":m '>+1<CR>gv=gv", opts)
 keymap("v", "K", ":m '<-2<CR>gv=gv",opts)
 
@@ -29,8 +35,8 @@ vim.cmd [[:amenu 10.100 mousemenu.Goto\ Definition <cmd>lua vim.lsp.buf.definiti
 vim.cmd [[:amenu 10.110 mousemenu.References <cmd>lua vim.lsp.buf.references()<CR>]]
 -- vim.cmd [[:amenu 10.120 mousemenu.-sep- *]]
 
-vim.keymap.set("n", "<RightMouse>", "<cmd>:popup mousemenu<CR>")
-vim.keymap.set("n", "<Tab>", "<cmd>:popup mousemenu<CR>")
+keymap("n", "<RightMouse>", "<cmd>:popup mousemenu<CR>")
+keymap("n", "<Tab>", "<cmd>:popup mousemenu<CR>")
 
 -- tailwind bearable to work with
 keymap({ "n", "x" }, "j", "gj", opts)
@@ -41,3 +47,21 @@ keymap("n", "<leader>w", ":lua vim.wo.wrap = not vim.wo.wrap<CR>", opts)
 vim.api.nvim_set_keymap('t', '<C-;>', '<C-\\><C-n>', opts)
 
 keymap("n", "<leader>pv",vim.cmd.Ex, opts)
+
+-- use jk to exit insert mode
+keymap("i", "jk", "<ESC>", opts)
+
+-- clear search highlights
+keymap("n", "<leader>nh", ":nohl<CR>", opts)
+keymap({ "i", "n" }, "<Esc>", "<Cmd>noh<CR><Esc>", opts)
+
+-- increment/decrement numbers
+keymap("n", "<leader>+", "<C-a>", opts) -- increment
+keymap("n", "<leader>-", "<C-x>", opts) -- decrement
+
+-- Replace word under cursor across entire buffer
+keymap('n', 'gB', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], opts)
+
+-- Select entire file
+keymap("n", "==", "gg<S-v>G", opts)
+
