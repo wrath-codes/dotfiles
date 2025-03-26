a503224c29caa3307e3fadb1eaa702b732439bca
Raphael Vaz <raphael@wrath.codes>
2025-03-26 15:43:16 -0300

    feat: fixed defined config, and readme with updated reference to command

--- nvim/.config/nvim/lua/wrath/vscode/autocmds.lua
+++ nvim/.config/nvim/lua/wrath/vscode/autocmds.lua
@@ -1,35 +1,43 @@
 -- VSCode-specific autocmds
 local vscode = require("vscode")
 
+local function notify_vscode_mode()
+    local mode = vim.api.nvim_get_mode().mode
+    local mode_name = ""
+
+    -- Convert Neovim mode to readable name
+    if mode == "n" then
+        mode_name = "normal"
+    elseif mode == "i" then
+        mode_name = "insert"
+    elseif mode == "v" then
+        mode_name = "visual"
+    elseif mode == "V" then
+        mode_name = "visual"  -- line visual
+    elseif mode == "\22" then -- Ctrl+V (block visual)
+        mode_name = "visual"  -- block visual
+    elseif mode == "c" then
+        mode_name = "cmdline"
+    elseif mode == "R" then
+        mode_name = "replace"
+    else
+        -- For other modes, just use as is
+        mode_name = mode
+    end
+
+    -- The correct way to call a command with arguments based on VSCode Neovim API docs
+    vscode.action("nvim-ui-plus.setMode", {
+        args = { mode = mode_name }
+    })
+end
+
 -- Mode change notification autocmd
 vim.api.nvim_create_autocmd("ModeChanged", {
     pattern = "*",
-    callback = function()
-        -- Get the current mode
-        local mode = vim.api.nvim_get_mode().mode
-        local mode_name = ""
+    callback = notify_vscode_mode,
+})
 
-        -- Convert Neovim mode to readable name
-        if mode == "n" then
-            mode_name = "normal"
-        elseif mode == "i" then
-            mode_name = "insert"
-        elseif mode == "v" then
-            mode_name = "visual"
-        elseif mode == "V" then
-            mode_name = "visual"  -- line visual
-        elseif mode == "\22" then -- Ctrl+V (block visual)
-            mode_name = "visual"  -- block visual
-        elseif mode == "c" then
-            mode_name = "cmdline"
-        elseif mode == "R" then
-            mode_name = "replace"
-        else
-            -- For other modes, just use as is
-            mode_name = mode
-        end
-
-        -- Use VSCodeNotify to call our extension command
-        vscode.action("nvim_ui_plus.setMode ", { mode = mode_name })
-    end,
+-- Notify on initial load
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
+    callback = notify_vscode_mode,
 })
