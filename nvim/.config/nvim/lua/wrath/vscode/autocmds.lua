9909075661008931cddef124708cd5a73d6aa4ed
Raphael Vaz <raphael@wrath.codes>
2025-03-27 15:15:24 -0300

    feat: - nothing, just autoformatted a file

--- nvim/.config/nvim/lua/wrath/vscode/autocmds.lua
+++ nvim/.config/nvim/lua/wrath/vscode/autocmds.lua
@@ -2,37 +2,37 @@
 local vscode = require("vscode")
 
 local function notify_vscode_mode()
-    local mode = vim.api.nvim_get_mode().mode
-    local mode_name = ""
+	local mode = vim.api.nvim_get_mode().mode
+	local mode_name = ""
 
-    -- Convert Neovim mode to readable name
-    if mode == "n" then
-        mode_name = "normal"
-    elseif mode == "i" then
-        mode_name = "insert"
-    elseif mode == "v" then
-        mode_name = "visual"
-    elseif mode == "V" then
-        mode_name = "visual"  -- line visual
-    elseif mode == "\22" then -- Ctrl+V (block visual)
-        mode_name = "visual"  -- block visual
-    elseif mode == "c" then
-        mode_name = "cmdline"
-    elseif mode == "R" then
-        mode_name = "replace"
-    else
-        -- For other modes, just use as is
-        mode_name = mode
-    end
+	-- Convert Neovim mode to readable name
+	if mode == "n" then
+		mode_name = "normal"
+	elseif mode == "i" then
+		mode_name = "insert"
+	elseif mode == "v" then
+		mode_name = "visual"
+	elseif mode == "V" then
+		mode_name = "visual" -- line visual
+	elseif mode == "\22" then -- Ctrl+V (block visual)
+		mode_name = "visual" -- block visual
+	elseif mode == "c" then
+		mode_name = "cmdline"
+	elseif mode == "R" then
+		mode_name = "replace"
+	else
+		-- For other modes, just use as is
+		mode_name = mode
+	end
 
-    -- The correct way to call a command with arguments based on VSCode Neovim API docs
-    vscode.action("nvim-ui-plus.setMode", {
-        args = { mode = mode_name }
-    })
+	-- The correct way to call a command with arguments based on VSCode Neovim API docs
+	vscode.action("nvim-ui-plus.setMode", {
+		args = { mode = mode_name },
+	})
 end
 
 -- Mode change notification autocmd
 vim.api.nvim_create_autocmd("ModeChanged", {
-    pattern = "*",
-    callback = notify_vscode_mode,
+	pattern = "*",
+	callback = notify_vscode_mode,
 })
